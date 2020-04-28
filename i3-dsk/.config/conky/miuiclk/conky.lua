37cdc1a38401e650a4e069c72f52d99e80b1ccd4
DiegoMC9 <dmartinezcapel@gmail.com>
2020-04-28 19:54:48 +0200

    future themes

--- i3-dsk/.config/conky/miuiclk/conky.lua
+++ i3-dsk/.config/conky/miuiclk/conky.lua
@@ -3,10 +3,7 @@ conky.config = {
 -- - Conky settings - #
 --#####################
 	update_interval = 1,
-	total_run_times = 0,
---	net_avg_samples = 1,
---	cpu_avg_samples = 1,
-
+	override_utf8_locale=true,
 	imlib_cache_size = 0,
 	double_buffer = true,
 	no_buffers = true,
@@ -15,9 +12,10 @@ conky.config = {
 -- - Text settings - #
 --####################
 	use_xft = true,
-	font = 'AvantGarde LT Medium:size=40',
+	xftalpha=1,
+	font="Source Code Pro, Light Italic:size=10",
 	override_utf8_locale = true,
-	text_buffer_size = 2048,
+	text_buffer_size = 128,
 
 --############################
 -- - Window specifications - #
@@ -26,41 +24,43 @@ conky.config = {
 	own_window_type = 'override',
 	own_window_transparent = true,
 	own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
---own_window_argb_visual yes
---own_window_argb_value 100
 
 	alignment = 'tm',
-	gap_x = 0,
-	gap_y = 30,
-	minimum_width = 350, minimum_height = 20,
-
+	gap_x = -25,
+	gap_y = 0,
+	minimum_width = 600,
+	minimum_height = 400,
 	default_bar_width = 60, default_bar_height = 0,
 
 --########################
 -- - Graphics settings - #
 --########################
 	draw_shades = false,
+	draw_shades=false,
+	draw_outline=false,
+	draw_borders=false,
 
 	default_color = '#080808',
-	default_shade_color = '#1d1d1d',
+	default_shade_color = '#000000',
 	color0 = '#ffffff',
 	color1 = '#ffffff',
-	color2 = '#ffffff',
-
 
 --lua_load ~/.conky/conkybg.lua
 --lua_draw_hook_pre conky_draw_bg
 
-	own_window_argb_visual = false,
-	own_window_colour = '#000000',
+	own_window_argb_visual = true,
+	--own_window_colour = '#000000',
 	own_window_argb_value = 0,
 };
 
 conky.text = [[
-${voffset 11} ${goto 30}${time %I} ${goto 110}${time %M}
-${image ./b.png -p 0,0 -s 188x100}
-${voffset -175} ${goto 226}${font AvantGarde LT Medium:size=10}${time %a} ${font}
-${font AvantGarde LT Medium:size=38}${voffset -65}${goto 215}${time %d}
-${image ./c.png -p 185,0 -s 100x100}
-
+	${font Comfortaa, Light:size=80}${color0}\
+	${voffset 30}\
+	${alignc}${time %I:%M}\
+	${font}${color}
+	\
+	${font Comfortaa, Light:size=40}${color0}\
+	${voffset 10}\
+	${alignc}${time %A, %B %d}\
+	${font}${color}
 ]];
--- i3-dsk/.config/polybar/scripts/colors.sh
+++ i3-dsk/.config/polybar/scripts/colors.sh
@@ -11,96 +11,50 @@ PID2="$(pgrep -u $UID -x polybar | tail -n 1)"
 LAUNCH1="polybar-msg -p $PID1 cmd restart"
 LAUNCH2="polybar-msg -p $PID2 cmd restart"
 
-## Alt..
+
 #LAUNCH="pkill -USR1 polybar"
 
-# Contrast
+# Love Death Robots-----------------------------------
 if  [[ $1 = "-mode1" ]]; then
 # Replacing colors
-sed -i -e 's/bg = .*/bg = #e1e1e1/g' $PDIR/colors.ini
-sed -i -e 's/fg = .*/fg = #f5f5f5/g' $PDIR/colors.ini
-sed -i -e 's/ac = .*/ac = #ffffff/g' $PDIR/colors.ini
-sed -i -e 's/bi = .*/bi = #dc404040/g' $PDIR/colors.ini
-sed -i -e 's/be = .*/be = #dc404040/g' $PDIR/colors.ini
-sed -i -e 's/mb = .*/mb = #0a0a0a/g' $PDIR/colors.ini
-sed -i -e 's/mgf = .*/mgf = #0a0a0a/g' $PDIR/colors.ini
-sed -i -e 's/mf = .*/mf = #fdd835/g' $PDIR/colors.ini
+sed -i -e 's/bg = .*/bg = #292929/g' $PDIR/colors.ini
+sed -i -e 's/fg = .*/fg = #e1f5fe/g' $PDIR/colors.ini
+sed -i -e 's/active = .*/active = #FD432E/g' $PDIR/colors.ini
+sed -i -e 's/mod-bg = .*/mod-bg = #AD1200/g' $PDIR/colors.ini
+sed -i -e 's/mgf = .*/mgf = #263238/g' $PDIR/colors.ini
+sed -i -e 's/mod-fg = .*/mod-fg = #FD432E/g' $PDIR/colors.ini
+sed -i -e 's/mod-fg-alt = .*/mod-fg-alt = #292929/g' $PDIR/colors.ini
 # Restarting polybar
 $LAUNCH1 &
 $LAUNCH2 &
-# Archlinux
+# Archlinux--------------------------------------------
 elif  [[ $1 = "-mode2" ]]; then
 # Replacing colors
 sed -i -e 's/bg = .*/bg = #37474F/g' $PDIR/colors.ini
 sed -i -e 's/fg = .*/fg = #DFDFDF/g' $PDIR/colors.ini
-sed -i -e 's/ac = .*/ac = #c2272d/g' $PDIR/colors.ini
+sed -i -e 's/active = .*/active = #c2272d/g' $PDIR/colors.ini
 sed -i -e 's/bi = .*/bi = #dc344148/g' $PDIR/colors.ini
 sed -i -e 's/be = .*/be = #dc344148/g' $PDIR/colors.ini
-sed -i -e 's/mb = .*/mb = #263238/g' $PDIR/colors.ini
-sed -i -e 's/mgf = .*/mgf = #263238/g' $PDIR/colors.ini
-sed -i -e 's/mf = .*/mf = #00BCD4/g' $PDIR/colors.ini
+sed -i -e 's/mod-bg = .*/mod-bg = #263238/g' $PDIR/colors.ini
+sed -i -e 's/mod-fg = .*/mod-fg = #00BCD4/g' $PDIR/colors.ini
 # Restarting polybar
 $LAUNCH1 &
 $LAUNCH2 &
-# Solarized
+# Manjaro
 elif  [[ $1 = "-mode3" ]]; then
 # Replacing colors
-sed -i -e 's/bg = .*/bg = #8c112526/g' $PDIR/colors.ini
-sed -i -e 's/fg = .*/fg = #C4AAA5/g' $PDIR/colors.ini
-sed -i -e 's/ac = .*/ac = #d26273/g' $PDIR/colors.ini
-sed -i -e 's/bi = .*/bi = #dc223637/g' $PDIR/colors.ini
-sed -i -e 's/be = .*/be = #dc223637/g' $PDIR/colors.ini
-sed -i -e 's/mb = .*/mb = #112526/g' $PDIR/colors.ini
-sed -i -e 's/mgf = .*/mgf = #112526/g' $PDIR/colors.ini
-sed -i -e 's/mf = .*/mf = #EE7313/g' $PDIR/colors.ini
-# Restarting polybar
-$LAUNCH1 &
-$LAUNCH2 &
-# Crimson
-elif  [[ $1 = "-mode4" ]]; then
-# Replacing colors
-sed -i -e 's/bg = .*/bg = #8c461320/g' $PDIR/colors.ini
-sed -i -e 's/fg = .*/fg = #f5f5f5/g' $PDIR/colors.ini
-sed -i -e 's/ac = .*/ac = #d26273/g' $PDIR/colors.ini
-sed -i -e 's/bi = .*/bi = #dc6A2939/g' $PDIR/colors.ini
-sed -i -e 's/be = .*/be = #dc6A2939/g' $PDIR/colors.ini
-sed -i -e 's/mb = .*/mb = #461320/g' $PDIR/colors.ini
-sed -i -e 's/mgf = .*/mgf = #461320/g' $PDIR/colors.ini
-sed -i -e 's/mf = .*/mf = #fdd835/g' $PDIR/colors.ini
-# Restarting polybar
-$LAUNCH1 &
-$LAUNCH2 &
-# Tropical
-elif  [[ $1 = "-mode5" ]]; then
-# Replacing colors
-sed -i -e 's/bg = .*/bg = #8c092F1C/g' $PDIR/colors.ini
-sed -i -e 's/fg = .*/fg = #131418/g' $PDIR/colors.ini
-sed -i -e 's/ac = .*/ac = #0084ff/g' $PDIR/colors.ini
-sed -i -e 's/bi = .*/bi = #dc206341/g' $PDIR/colors.ini
-sed -i -e 's/be = .*/be = #dc206341/g' $PDIR/colors.ini
-sed -i -e 's/mb = .*/mb = #00c300/g' $PDIR/colors.ini
-sed -i -e 's/mgf = .*/mgf = #00c300/g' $PDIR/colors.ini
-sed -i -e 's/mf = .*/mf = #ea4c89/g' $PDIR/colors.ini
-# Restarting polybar
-$LAUNCH1 &
-$LAUNCH2 &
-# Aquamarine
-elif  [[ $1 = "-mode6" ]]; then
-# Replacing colors
-sed -i -e 's/bg = .*/bg = #8c003C3C/g' $PDIR/colors.ini
-sed -i -e 's/fg = .*/fg = #CFCFCF/g' $PDIR/colors.ini
-sed -i -e 's/ac = .*/ac = #d26273/g' $PDIR/colors.ini
-sed -i -e 's/bi = .*/bi = #dc085F5F/g' $PDIR/colors.ini
-sed -i -e 's/be = .*/be = #dc085F5F/g' $PDIR/colors.ini
-sed -i -e 's/mb = .*/mb = #003C3C/g' $PDIR/colors.ini
-sed -i -e 's/mgf = .*/mgf = #003C3C/g' $PDIR/colors.ini
-sed -i -e 's/mf = .*/mf = #00acc1/g' $PDIR/colors.ini
+sed -i -e 's/bg = .*/bg = #37474f/g' $PDIR/colors.ini
+sed -i -e 's/fg = .*/fg = #e1f5fe/g' $PDIR/colors.ini
+sed -i -e 's/active = .*/active = #03a9f4/g' $PDIR/colors.ini
+sed -i -e 's/mod-bg = .*/mod-bg = #37474f/g' $PDIR/colors.ini
+sed -i -e 's/mgf = .*/mgf = #263238/g' $PDIR/colors.ini
+sed -i -e 's/mod-fg = .*/mod-fg = #00BCD4/g' $PDIR/colors.ini
+sed -i -e 's/mod-fg-alt = .*/mod-fg-alt = #00BCD4/g' $PDIR/colors.ini
 # Restarting polybar
 $LAUNCH1 &
 $LAUNCH2 &
 
 else
 echo "Available options:
--mode1		-mode2			-mode3
--mode4		-mode5			-mode6"
+-mode1		-mode2			-mode3"
 fi
