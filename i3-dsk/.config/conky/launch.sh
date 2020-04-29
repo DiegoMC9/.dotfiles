0ac1fea6f9c812229bb58dcf52aa1635f5f6d01f
DiegoMC9 <dmartinezcapel@gmail.com>
2020-04-29 11:38:59 +0200

    conky finished, polybar wip, rofi wip

--- i3-dsk/.config/conky/launch.sh
+++ i3-dsk/.config/conky/launch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
-cwd=$HOME/.config/conky
-theme=miuiclk
+dir=$HOME/.config/conky/themes
+#Gotham, LDR
+theme=LDR
 
 killall -q conky
 
 while pgrep -u $UID -x conky >/dev/null; do sleep 1; done
-cd $cwd/$theme
+cd $dir/$theme
 conky -d -o -c ./conky.lua
